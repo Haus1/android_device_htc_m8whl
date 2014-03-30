85f712a31752175cad5c75cd9504d027619f789e
Simon Sickle <simon@simonsickle.com>
2014-03-30 11:37:26 -0400

    Update device_m8.mk

--- device_m8.mk
+++ device_m8.mk
@@ -72,7 +72,6 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, build/target/product/full.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_m8
 PRODUCT_DEVICE := m8
 
