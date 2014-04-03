f3d8bbc84b6380ee7739af4146790239355840e4
Simon Sickle <simon@simonsickle.com>
2014-04-03 17:18:16 -0400

    Update omni_m8whl.mk

--- omni_m8whl.mk
+++ omni_m8whl.mk
@@ -1,16 +1,14 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := m8_whl
-
-# Inherit some common DU stuff.
+# Omni Common
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/m8_whl/device_m8_whl.mk)
 
 ## Device identifier. This must come after all inclusions
+PRODUCT_RELEASE_NAME := m8whl
 PRODUCT_DEVICE := m8whl
 PRODUCT_NAME := omni_m8whl
 PRODUCT_BRAND := htc
