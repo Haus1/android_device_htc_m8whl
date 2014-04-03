b756d77e73de868a50920ad1dada6b082d04dea3
Simon Sickle <simon@simonsickle.com>
2014-04-03 17:07:05 -0400

    Fix naming

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter m8_whl,$(TARGET_DEVICE)),)
+ifneq ($(filter m8whl,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
