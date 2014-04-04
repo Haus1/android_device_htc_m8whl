a7ca1f414de1218a860ad4114bd14286683793c0
Simon Sickle <simon@simonsickle.com>
2014-04-03 20:21:41 -0400

    Fix links

--- omni_m8whl.mk
+++ omni_m8whl.mk
@@ -5,7 +5,7 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/m8_whl/device_m8_whl.mk)
+$(call inherit-product, device/htc/m8whl/device_m8whl.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_RELEASE_NAME := m8whl
