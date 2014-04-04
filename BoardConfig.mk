6456f9577ba3f67dcca665997fbcc0221434860d
Simon Sickle <simon@simonsickle.com>
2014-04-03 20:27:27 -0400

    Disable sweep to unlock

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,3 +50,4 @@ TW_EXTERNAL_STORAGE_MOUNT_POINT := "usb-otg"
 #TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 #TW_MAX_BRIGHTNESS := 255
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_NO_SCREEN_BLANK := true
